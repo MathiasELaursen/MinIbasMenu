8486fb45ce39c43e79156f79a3a1370196be00a6
Mathias Laursen <97mathias@live.dk>
2022-10-06 11:39:22 +0200

    v2

---
+++ Pages/Shared/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IBAS_menu.Pages.Shared
+{
+    public class informationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
